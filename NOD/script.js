ba628e35e8e7db003393b8d344df0bf22c4beb97
Sergey Zaligin <webdevjsphp@gmail.com>
2018-10-30 07:04:14 +0300

    fix

--- NOD/script.js
+++ NOD/script.js
@@ -1,6 +1,24 @@
 function nod () {
+
 	let a = parseInt(prompt('a='));
 	let b = parseInt(prompt('b='));
+	let min = undefined;
+
+	if (a === b) {
+		alert(`NODE=${a}`);
+		return true;
+	}
+
+	if (a > b) {
+		min = b;
+	} else {
+		min = a;
+	}
+
+	for (let i = min; i >= 0; i--) {
+		
+	}
+
 	console.log('a', a);
 	console.log('b', b);
 }
