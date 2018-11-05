52f7c96faa966034bc0cc8634c17774df9dd7116
Sergey Zaligin <webdevjsphp@gmail.com>
2018-11-05 10:50:26 +0300

    fix

--- GCD/script.js
+++ GCD/script.js
@@ -1,11 +1,12 @@
 function gcd () {
 
-	let a = parseInt(prompt('a='));
-	let b = parseInt(prompt('b='));
+	let a = parseInt(prompt('Input number a = '));
+	let b = parseInt(prompt('Input number b = '));
 	let min = undefined;
+	let gcd = 1;
 
 	if (a === b) {
-		alert(`Greatest common divisor=${a}`);
+		alert(`Greatest common divisor = ${a}`);
 		return true;
 	}
 
@@ -16,8 +17,14 @@ function gcd () {
 	}
 
 	for (let i = min; i >= 0; i--) {
-		
+		if (a%i === 0 && b%i === 0) {
+			gcd = i
+			break;
+		}
+
 	}
+
+	alert(`Greatest common divisor = ${ gcd }`);
 
 	console.log('a', a);
 	console.log('b', b);
