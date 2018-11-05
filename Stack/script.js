ed04e355f4d9983b38c8ce25a19d229fc4b73bc3
Sergey Zaligin <webdevjsphp@gmail.com>
2018-11-05 09:47:16 +0300

    fix

---
+++ Stack/script.js
@@ -0,0 +1,23 @@
+/**
+ * Вспомогательный класс содержит:
+ * value -значение элемента списка и 
+ * next - ссылку на следующий элемент
+ */
+class StackNode {
+	constructoor (value) {
+		this.value = value;
+		this.next = null;
+	}
+}
+
+/**
+ * Класс Stack
+ * root - корень 
+ * n - кол-во элементов
+ */
+class Stack {
+	constructoor () {
+		this.root = null;
+		this.n = null;
+	}
+}
